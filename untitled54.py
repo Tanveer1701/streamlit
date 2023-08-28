61bdf2aba4c57b4e745aee1d903898f98e965c88
Tanveer Hassan <72239392+Tanveer1701@users.noreply.github.com>
2023-08-28 15:40:56 +0530

    Update untitled54.py

--- untitled54.py
+++ untitled54.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1Lts0rorpVfTQ55RiXSYclqwL1GJZJ_Pp
 """
 
-!pip install streamlit
+
 import streamlit as st
 
 def find_largest(num1, num2, num3):
